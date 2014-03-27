846ed21a6e4a7d0ffeae65db8848d23ee5846410
Ryo TANAKA <ryotan@ucb-jp.net>
2014-03-28 00:03:11 +0900

    Products list ページを作れた。

--- products/app/controllers/Application.scala
+++ products/app/controllers/Application.scala
@@ -1,13 +1,11 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
-import play.api.i18n.Messages
 
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index(s"Your new application is ready. Application name is ${Messages("application.name")}"))
+    Redirect(routes.Products.list())
   }
 
 }
