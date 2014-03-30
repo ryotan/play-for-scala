0ebf4479431af69b9ad37e6f58b31959697c9b13
Ryo TANAKA <ryotan@ucb-jp.net>
2014-03-30 14:15:57 +0900

    新規登録画面を作ってみたよ。

--- products/app/controllers/Products.scala
+++ products/app/controllers/Products.scala
@@ -1,7 +1,12 @@
 package controllers
 
-import play.api.mvc.{Action, Controller}
+import play.api.mvc.{Flash, Action, Controller}
 import models.Product
+import play.api.data.Form
+
+import play.api.data.Forms.{mapping, longNumber, nonEmptyText}
+import play.api.i18n.Messages
+
 
 object Products extends Controller {
   def list = Action { implicit request =>
@@ -16,5 +21,40 @@ object Products extends Controller {
       Ok(views.html.products.details(product))
     }.getOrElse(NotFound)
   }
+
+  def save = Action { implicit request =>
+    val newProductForm = productForm.bindFromRequest()
+
+    newProductForm.fold(
+      hasErrors = { form =>
+        Redirect(routes.Products.newProduct())
+          .flashing(Flash(form.data) + ("error" -> Messages("validation.errors")))
+      },
+
+      success = { newProduct =>
+        Product.add(newProduct)
+        Redirect(routes.Products.show(newProduct.ean))
+          .flashing("success" -> Messages("products.new.success", newProduct.name))
+      }
+    )
+  }
+
+  def newProduct = Action {implicit request =>
+    val form = if (flash.get("error").isDefined)
+      productForm.bind(flash.data)
+    else {
+      productForm
+    }
+
+    Ok(views.html.products.edit(form))
+  }
+
+  private val productForm: Form[Product] = Form (
+    mapping(
+      "ean" -> longNumber.verifying("validation.ean.duplicate", Product.findByEan(_).isEmpty),
+      "name" -> nonEmptyText,
+      "description" -> nonEmptyText
+    ) (Product.apply) (Product.unapply)
+  )
 
 }
