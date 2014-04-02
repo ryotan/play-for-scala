cb1797a4815a6c39b2cb1738d5c8e778e69c0f28
Ryo TANAKA <ryotan@ucb-jp.net>
2014-04-02 23:13:17 +0900

    テストケースを少し追加。

--- yabe/test/models/UserSpec.scala
+++ yabe/test/models/UserSpec.scala
@@ -13,10 +13,15 @@ class UserSpec extends Specification {
   "User" should {
     "save user information" in new WithApplication {
       new User("bob@example.com", "secret", "Bob", isAdmin = false).save()
+      new User("admin@example.com", "secret", "Admin", isAdmin = true).save()
 
       val bob = User.findByEmail("bob@example.com").get
       bob.email === "bob@example.com"
       bob.fullname === "Bob"
+      bob.isAdmin === false
+
+      val admin = User.findByEmail("admin@example.com").get
+      admin.isAdmin === true
     }
 
     "provide connection as a user" in new WithApplication {
