f64768731c0b2e98caf5360f56914c4ac9e84b25
Ryo TANAKA <ryotan@ucb-jp.net>
2014-04-15 23:19:19 +0900

    Add comment form.

--- yabe/app/controllers/Application.scala
+++ yabe/app/controllers/Application.scala
@@ -2,6 +2,8 @@ package controllers
 
 import play.api.mvc._
 import models.Post
+import play.api.data._
+import play.api.data.Forms._
 
 object Application extends Controller {
 
@@ -11,7 +13,25 @@ object Application extends Controller {
 
   def show(id: Long) = Action {
     val post = Post.findById(id).get
-    Ok(views.html.show(post, post.previous(), post.next()))
+    Ok(views.html.show(post, post.previous(), post.next(), commentForm))
+  }
+
+  val commentForm = Form(
+    mapping(
+      "author" -> nonEmptyText,
+      "content" -> nonEmptyText
+    )(CommentData.apply)(CommentData.unapply)
+  )
+
+  def postComment(postId: Long) = Action { implicit request =>
+    val commentData = commentForm.bindFromRequest.get
+
+    val post = Post.findById(postId).get
+    post.addComment(commentData.author, commentData.content)
+    Redirect(routes.Application.show(postId))
   }
 
 }
+
+case class CommentData(author: String, content: String)
+
